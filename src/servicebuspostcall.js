fbea75f93d3001d87ab9d41e8c60df0da1c450e2
Nithesh-STACK <nitheshvilla@gmail.com>
2024-06-17 22:40:44 +0530

    changes

--- src/servicebuspostcall.js
+++ src/servicebuspostcall.js
@@ -10,23 +10,38 @@ const ProcessIdInvokeCall=()=>{
 
     //SERVICE BUS IMPLEMENTATION
     const newUuid = uuid()
-let headers =
-{"Authorization":"SharedAccessSignature sr=https%3A%2F%2FRASServiceBus.servicebus.windows.net%2Frasqueue&sig=kDzOj04Wt5NI5HbgDKdEa4KiY38UsetkxVDFovUb27E%3D&se=1719216470&skn=queuepolicy"};
-let data={
+// let headers =
+// {"Authorization":"SharedAccessSignature sr=https%3A%2F%2FRASServiceBus.servicebus.windows.net%2Frasqueue&sig=kDzOj04Wt5NI5HbgDKdEa4KiY38UsetkxVDFovUb27E%3D&se=1719216470&skn=queuepolicy"};
+// let data={
 
+//         "id" : newUuid,
+//         "functionality" :"Download",
+//         "data" : 123
+    
+// }
+fetch('https://RASServiceBus.servicebus.windows.net/rasqueue/messages', {
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+      'Authorization':"SharedAccessSignature sr=https%3A%2F%2FRASServiceBus.servicebus.windows.net%2Frasqueue&sig=kDzOj04Wt5NI5HbgDKdEa4KiY38UsetkxVDFovUb27E%3D&se=1719216470&skn=queuepolicy"
+
+    },
+    body: JSON.stringify({
         "id" : newUuid,
         "functionality" :"Download",
         "data" : 123
-    
+    })
+  })
 }
-let url="https://RASServiceBus.servicebus.windows.net/rasqueue/messages";
-let res=fetch(url,data,{headers:headers}).then((data)=>{
-console.log("success",data);
-}).catch((e)=>{
-console.log(e);
-})
-console.log(res);
-}
+// let url="https://RASServiceBus.servicebus.windows.net/rasqueue/messages";
+// let res=(url,data,{headers:headers}).then((data)=>{
+// console.log("success",data);
+// }).catch((e)=>{
+// console.log(e);
+// })
+// console.log(res);
+// }
 return  <div className="container">
 <div className="register-container"><p>Register User:</p>
 <button className="register-btn">Register</button></div>
