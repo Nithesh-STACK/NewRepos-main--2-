107309ffc2c869f77fe3ea877c7040daed2ebf12
Nithesh Stephen <76839916+Nithesh-STACK@users.noreply.github.com>
2024-06-18 19:21:36 +0530

    Update servicebuspostcall.js

--- src/servicebuspostcall.js
+++ src/servicebuspostcall.js
@@ -1,10 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import { v4 as uuid } from "uuid";
+import * as signalR from "@microsoft/signalr";
 
 const ServiceCall=()=>{
-// const RegisterUser=()=>{
-//     //SIGNAL R IMPLEMENTATION
-// }
+  const [connId,setConn]=useState('');
+const RegisterUser=async()=>{
+    //SIGNAL R IMPLEMENTATION
+    const connection = new signalR.HubConnectionBuilder()       
+    .withUrl('https://signalrnotification20240618165829.azurewebsites.net/notificationHub',
+       {
+         skipNegotiation: true,
+       transport: signalR.HttpTransportType.WebSockets
+       }
+ )       
+   .build();
+
+   connection.on("RegisterUser",(message)=> {
+    setConn(message);
+    alert(message);
+});
+
+
+connection.on("ReceiveNotification",(message)=> {
+   console.log("Message received: " + message);
+   alert(message);
+});
+
+
+
+
+
+await connection.start()
+// Replace 'user123' with the unique user ID
+
+await connection.invoke("RegisterUser", "user123");
+}
 
 const ProcessIdInvokeCall=()=>{
 
@@ -29,8 +59,7 @@ fetch('https://RASServiceBus.servicebus.windows.net/rasqueue/messages', {
     },
     body: JSON.stringify({
         "id" : newUuid,
-        "functionality" :"Download",
-        "data" : 123
+        "connectionId":connId
     })
   })
 }
@@ -44,7 +73,7 @@ fetch('https://RASServiceBus.servicebus.windows.net/rasqueue/messages', {
 // }
 return  <div className="container">
 <div className="register-container"><p>Register User:</p>
-<button className="register-btn">Register</button></div>
+<button className="register-btn" onClick={RegisterUser}>Register</button></div>
 <div className="process-container">
 <p>ProcessId Initiate</p>
 <button className="process-btn" onClick={ProcessIdInvokeCall}>SendProcessId</button>
